2c6388bdb303bf9635077b284904e8cbfaf434b9
Nurfihartono <nurfihartonosaflin@gmail.com>
2025-06-18 19:43:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sistem_management_buku_valid
---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <h1>Sistem Management Buku</h1>
+
+        <ul>
+            <li>Dashboard</li>
+            <li>Management Buku</li>
+            <li>Management Pengguna</li>
+        </ul>
+    </nav>
+</body>
+</html>
