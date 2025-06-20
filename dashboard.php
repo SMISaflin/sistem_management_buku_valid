857b24bd11edabfb2b1952bbb3bb029c4097a541
Nirmalanina <malamalaaa00@gmail.com>
2025-06-20 17:04:49 +0800

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -7,14 +7,49 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <nav>
-        <h1>Sistem Management Buku</h1>
+    <div class="container">
+        <nav class="sidebar">
+            <h1>Sistem Management Buku</h1>
+            <header>
+                <ul>
+                    <li><a href="dashboard.php">Dashboard</a></li>
+                    <li><a href="management_buku.php">Management Buku</a></li>
+                    <li><a href="management_pengguna.php">Management Pengguna</a></li>
+                    <li><a href="logout.php">Log Out</a></li> 
+                </ul>
+            </header>
+        </nav>
+        <div class="tombol">
+            <h3>Dashboard</h3>
+            <nav class="cari">
+                <button>Cari</button>
+                <input type="Cari Buku" placeholder="Cari Buku"></input>
+            </nav>
+        <div class="card-container">
+            <div class="cards">
+                <div class="card biru">
+                    <h2>Total Buku</h2>
+                    <p>350 Buku</p>
+                </div>
+                <div class="card lilac">
+                    <h2>Anggota</h2>
+                    <p>300 Orang</p>
+                </div>
+                <div class="card lilac">
+                    <h2>Kategori</h2>
+                    <p>10 Kategori</p>
+                </div>
+                <div class="card biru">
+                    <h2>Peminjam Aktif</h2>
+                    <p>100 Orang</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
 
-        <ul>
-            <li>Dashboard</li>
-            <li>Management Buku</li>
-            <li>Management Pengguna</li>
-        </ul>
-    </nav>
+    <footer>
+        <p>&copy;2025. Semua Hak Cipta Dilindungi Oleh Pihak Berwajib.</p>
+    </footer>
 </body>
 </html>
